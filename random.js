f8b1f1d5169845e2bdd2fa4ad9bd1fff149a8ef5
JAVIER GRANJA <javier.granja@gmail.com>
2015-06-23 10:22:20 +0200

    random versión 2

--- random.js
+++ random.js
@@ -1,10 +1,8 @@
 // Math.random() devuelve un número aleatorio entre 0 y 1
 var numero =Math.random();
+var str='\n MAYOR que 0,5\n';
 
 if (numero<0.5) {
-	console.log(numero+ '\n MENOR que 0,5\n');
-} 
-else{
-
-	console.log(numero+ '\nMAYOR que 0,5\n');
+str='\n MENOR que 0,5\n';
 };
+console.log(numero+str);
